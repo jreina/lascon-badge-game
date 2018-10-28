62a2675e7bf5d81186141ef763b5d75fc2f70fea
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-28 13:14:54 -0500

    still don't know

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const fs = require('fs');
 const base64 = require('base-64');
 
 const message = fs.readFileSync('message.txt', { encoding: 'utf8' });
+const lines = message.split('\r\n');
 
-message
-  .split('\r\n')
-  .map(base64.decode)
-  .forEach(console.log);
+const log = x => console.log(x);
+const add = (a, b) => a + b;
+const pos = index => traversable => traversable[index];
+
+// Welcome to the LASCON 2018 badge game!
+// lines.map(base64.decode).forEach(log);
+
+// FEHGFFFEFFEFFHFFFEHFFGGGFEGFEHEGFEHEFGGGFGFHEHFFFEHEFGGGFEFFFHGFFEHFFEEFEHHFEHHF
+// log(lines.map(pos(50)).reduce(add));
