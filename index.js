65b9dcc86f6566e889595926fb399de66194ebab
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-28 23:15:30 -0500

    Adds enough JavaScript to solve the first step.

--- index.js
+++ index.js
@@ -7,9 +7,41 @@ const lines = message.split('\r\n');
 const log = x => console.log(x);
 const add = (a, b) => a + b;
 const pos = index => traversable => traversable[index];
-
-// Welcome to the LASCON 2018 badge game!
-// lines.map(base64.decode).forEach(log);
+const prop1 = obj => key => obj[key];
+const last = arr => arr[arr.length - 1];
+const chunk = (n, arr) =>
+  arr.reduce(
+    (memo, val) => {
+      if (last(memo).length === n) memo.push([]);
+      last(memo).push(val);
+      return memo;
+    },
+    [[]]
+  );
+const tap = f => x => {
+  f(x);
+  return x;
+};
+const bin2Ascii = str =>
+  chunk(8, [...str])
+    .map(x => x.join(''))
+    .map(arr => Number.parseInt(arr, 2))
+    .map(charCode => String.fromCharCode(charCode))
+    .join('');
+const mappings = { E: '00', F: '01', G: '10', H: '11' };
 
 // FEHGFFFEFFEFFHFFFEHFFGGGFEGFEHEGFEHEFGGGFGFHEHFFFEHEFGGGFEFFFHGFFEHFFEEFEHHFEHHF
-// log(lines.map(pos(50)).reduce(add));
+const EFGH = lines.map(pos(50)).reduce(add);
+log(EFGH);
+
+// 0100111001010100010100010111010101001101011010100100100100110010010011000110101001100111001101010100110001101010010001010111100101001101010000010011110100111101
+const bin = [...EFGH].map(prop1(mappings)).reduce(add);
+log(bin);
+
+//NTQuMjI2Ljg5LjEyMA==
+const ascii = bin2Ascii(bin);
+console.log(ascii);
+
+// 54.226.89.120
+const ipAddr = base64.decode(ascii);
+console.log(ipAddr);
