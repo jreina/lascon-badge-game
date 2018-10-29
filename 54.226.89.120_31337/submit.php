0bd73c2f1222e4c9b42d41fd4683ddcc212870cb
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-28 23:15:07 -0500

    Adds the last step page

---
+++ 54.226.89.120_31337/submit.php
@@ -0,0 +1,23 @@
+<HTML>
+<BODY>
+
+<h1>LASCON Badge Game Login</h1>
+<mark>If you're trying to brute force this login form, you're playing the game wrong.</mark><br>
+<br>
+<form method="post" name="winner_submission">
+<table>
+<tr>
+<td>Username:&nbsp;</td><td><input type="text" name="username" /></td>
+</tr>
+<tr>
+<td>Password:&nbsp;</td><td><input type="password" name="password" /></td>
+</tr>
+<tr>
+<td colspan=2><input type="submit" name="winner_submission" value="LOGIN" /></td>
+</tr>
+</table>
+</form>
+<!-- Make sure to change the username to something other than "lascon" -->
+
+</BODY>
+</HTML>
